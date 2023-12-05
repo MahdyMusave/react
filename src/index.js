f4a3e921b2b9667ca8d5a6bd8ab9993bbd0da914
mahde13 <musavemahdy>
2023-12-05 04:54:09 -0900

    install json-serevr and create db.json and connect to localhost:3000/type

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
+import ReactDOM from "react-dom";
 import App from "./app";
-import ReactDOM from "react-dom/client";
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
