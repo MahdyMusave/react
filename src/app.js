540ca2f3be33e459c9e51d0d4b22afb2dc9df51d
mahde13 <musavemahdy>
2023-12-05 20:30:41 -0900

    create memory game with react

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
-import TripList from "./component/tripList";
+
 import "./app.css";
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
+
 const App = () => {
   return (
     <>
-      <TripList />
+      <div className="App">
+        <h1>Memory game</h1>
+        <button>New Game</button>
+      </div>
     </>
   );
 };
